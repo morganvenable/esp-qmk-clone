26944ddd261ca5f3b782aaf62209142fd33f89f8
Ben Gruver <jesusfreke@jesusfreke.com>
2021-03-31 18:49:49 -0700

    Add tinyusb and hook everything up

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -12,38 +12,25 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "esp_log.h"
+
+#include "bsp/board.h"
 
 #include "keyboard.h"
 
+#include "gpio.h"
+
+
+int idf_main(void);
+
 void app_main(void)
 {
-    printf("Hello world!\n");
+    //board_init();
+    //tud_task();
+    idf_main();
+}
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
-            CONFIG_IDF_TARGET,
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-
-
-    keyboard_task();
-
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+void blah() {
+    board_init();
+    tud_task();
 }
